1a2e6491fc56cf684d7b1ea4c745484afa886076
Skepperonii <103543001+SaucySlashing@users.noreply.github.com>
2023-06-14 18:35:19 -0600

    no

--- Assets/AbsolutelyBeatenToDeath.cs
+++ Assets/AbsolutelyBeatenToDeath.cs
@@ -14,7 +14,7 @@ public class AbsolutelyBeatenToDeath : MonoBehaviour
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Hazard")
         {
             SceneManager.LoadScene(0);
         }
--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
@@ -20,5 +21,13 @@ public class Teleport : MonoBehaviour
     public void subtract()
     {
         enemyCount -= 1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && enemyCount == 0)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
